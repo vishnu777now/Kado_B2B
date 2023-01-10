f8a1b244161d2d23403b227d3dd975ed43fbbfc9
sahilnarula90 <110301889+sahilnarula90@users.noreply.github.com>
2023-01-10 14:00:28 +0530

    changes

---
+++ assets/js/b2b_custom.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+	$("body").on('click', '.toggle-password', function() {
+		$('.toggle-password').toggleClass("fa-eye-slash fa-eye");
+		var input = $(".account_field_pass");
+		if (input.attr("type") === "password") {
+		  input.attr("type", "text");
+		} else {
+		  input.attr("type", "password");
+		}
+		
+		var inputcvv_payment = $(".cvv_payment");
+		if (inputcvv_payment.attr("type") === "password") {
+		  inputcvv_payment.attr("type", "number");
+		} else {
+		  inputcvv_payment.attr("type", "password");
+		}
+		
+		
+	  });
+	
+	$('.create_acc_box input').val("");
+	  $('.create_acc_box input').focusout(function() {
+		var text_val = $(this).val();
+		if (text_val === "") {
+		  console.log("empty!");
+		  $(this).removeClass('has-value');
+		} else {
+		  $(this).addClass('has-value');
+		}
+	});
+
+	$(".finder-second-age-input label").click(function () {
+		$(".finder-second-age-input label").removeClass('label-bg');
+	 	$(this).addClass('label-bg');
+	});
+	
+	$(".finder-second-label").click(function () {
+		$(".forward_outer ").show();
+		$(".ques_three_boxes").hide();
+	});
+	
+	$(".finder-first-label").click(function () {
+		$(".forward_outer ").hide();
+		$(".ques_three_boxes").show();
+	});
+	
+	
+	$(".que_one_boxes").each(function (){
+	  $(this).click(function(){
+		clearStylenew();
+		$(this).addClass("active");
+	  });
+	});
+	function clearStylenew(){
+	  buttonWithActive = $('.que_one_boxes.active');
+	  buttonWithActive.removeClass('active');
+	}
+	
+	$(".ques_three_main").each(function (){
+	  $(this).click(function(){
+		clearStyle();
+		$(this).addClass("active");
+	  });
+	});
+	function clearStyle(){
+	  buttonWithActive = $('.ques_three_main.active');
+	  buttonWithActive.removeClass('active');
+	}	
+	 
+	$('.help_outer h2 a').click(function(){
+		$(this).parent().next().slideToggle();
+		$(this).parent().toggleClass('help_active');
+		
+	});
+	 
+	 
+	 
+});
